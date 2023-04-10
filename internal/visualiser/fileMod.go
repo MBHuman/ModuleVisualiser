004fc9d762dcef0b053223eae59ca94fd2c3414d
Evgeny <zenka712@mail.ru>
2023-04-11 01:36:52 +0300

    add tests for fileMod

--- internal/visualiser/fileMod.go
+++ internal/visualiser/fileMod.go
@@ -2,37 +2,42 @@ package visualiser
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
 )
 
 type FileMod struct {
-	filePath        string
-	deps            map[string][]string
-	requirementRoot *Requirement
+	filePath    string
+	deps        map[string][]string
+	requirement *Requirement
 }
 
+// Create new fileMod object, automaticaly extracts data from file
+// and build requirement graph
 func NewFileMod(filePath string) (*FileMod, error) {
 	fileMod := &FileMod{
-		filePath:        filePath,
-		deps:            make(map[string][]string),
-		requirementRoot: NewRequirement(),
+		filePath:    filePath,
+		deps:        make(map[string][]string),
+		requirement: NewRequirement(),
 	}
 	if err := fileMod.extractDataFromFile(); err != nil {
 		return nil, err
 	}
-	if err := fileMod.getRequirements(); err != nil {
+	if err := fileMod.buildRequirements(); err != nil {
 		return nil, err
 	}
 	return fileMod, nil
 }
 
-func (f *FileMod) extractDataFromFile() error {
-	file, err := os.Open(f.filePath)
+// Extract data from file
+func (fileMod *FileMod) extractDataFromFile() error {
+	file, err := os.Open(fileMod.filePath)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
+	isDirect := true
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -48,21 +53,59 @@ func (f *FileMod) extractDataFromFile() error {
 				if !strings.HasPrefix(line, "\t") {
 					// The line contains the package name and version
 					pkg := strings.Split(line, " ")[0]
-					f.deps[pkg] = nil
+					fileMod.deps[pkg] = nil
 				} else {
 					// The line contains a dependency of a package
 					dep := strings.TrimSpace(line)
-					lastPkg := strings.Split(dep, " ")[len(strings.Split(dep, " "))-1]
-					f.deps[lastPkg] = append(f.deps[lastPkg], dep)
+					lastPkg := "indirect"
+					if isDirect {
+						lastPkg = "direct"
+					}
+					fileMod.deps[lastPkg] = append(fileMod.deps[lastPkg], dep)
 				}
 			}
+			isDirect = !isDirect
 		}
 	}
 
 	return nil
 }
 
-func (f *FileMod) getRequirements() error {
+// get All elements of fileMode.requirement
+func (fileMode *FileMod) getChilds() (map[string][]string, error) {
+	elements := make([]*RequirementNode, 0, CAP_SIZE)
+	childs := make(map[string][]string, CAP_SIZE)
+	elements = append(elements, fileMode.requirement.root)
 
+	// classical BFS
+	for len(elements) > 0 {
+		cur := elements[0]
+		elements = elements[1:]
+		if len(cur.childs) > 0 {
+			childs[cur.url] = make([]string, 0, CAP_SIZE)
+		}
+		for _, child := range fileMode.requirement.elementsSet[cur.url].childs {
+			childs[cur.url] = append(childs[cur.url], child.url)
+			elements = append(elements, child)
+		}
+	}
+
+	return childs, nil
+}
+
+// build Requirements tree from requirements in file
+// :TODO remove pkg != "indirect" and add indirect deps dependency on direct deps
+func (fileMod *FileMod) buildRequirements() error {
+	root := fileMod.requirement
+	for pkg, deps := range fileMod.deps {
+		if pkg != "indirect" {
+			for _, dep := range deps {
+				if len(strings.Split(dep, " ")) == 0 {
+					return fmt.Errorf("DEP STRING HAS LENGTH 0")
+				}
+				root.AddSingleRequirement(strings.TrimSpace(strings.Split(dep, " ")[0]))
+			}
+		}
+	}
 	return nil
 }
--- internal/visualiser/fileMod_test.go
+++ internal/visualiser/fileMod_test.go
@@ -1,6 +1,7 @@
 package visualiser
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/smartystreets/goconvey/convey"
@@ -8,7 +9,11 @@ import (
 
 func TestFileMod_extractDataFromFile(t *testing.T) {
 	convey.Convey("Given a FileMod instance with a file containing dependencies", t, func() {
-		fileMod := &FileMod{filePath: "testdata/testmod.mod", deps: make(map[string][]string)}
+		fileMod := &FileMod{
+			filePath:    "testdata/testmod.mod",
+			deps:        make(map[string][]string),
+			requirement: NewRequirement(),
+		}
 
 		convey.Convey("When extractDataFromFile is called", func() {
 			err := fileMod.extractDataFromFile()
@@ -38,14 +43,78 @@ func TestFileMod_extractDataFromFile(t *testing.T) {
 						"gopkg.in/ini.v1 v1.67.0 // indirect",
 						"gopkg.in/yaml.v3 v3.0.1 // indirect",
 					},
-					"v1.0.0":  {"github.com/jackpal/bencode-go v1.0.0"},
-					"v1.15.0": {"github.com/spf13/viper v1.15.0"},
-					"v1.6.1":  {"github.com/spf13/cobra v1.6.1"},
-					"v1.8.1":  {"github.com/stretchr/testify v1.8.1"},
+					"direct": {
+						"github.com/jackpal/bencode-go v1.0.0",
+						"github.com/spf13/viper v1.15.0",
+						"github.com/spf13/cobra v1.6.1",
+						"github.com/stretchr/testify v1.8.1",
+					},
+				}
+				if _, ok := expectedDeps["indirect"]; ok {
+					sort.Strings(expectedDeps["indirect"])
+				}
+				if _, ok := expectedDeps["direct"]; ok {
+					sort.Strings(expectedDeps["direct"])
+				}
+
+				if _, ok := fileMod.deps["indirect"]; ok {
+					sort.Strings(fileMod.deps["indirect"])
+				}
+				if _, ok := expectedDeps["direct"]; ok {
+					sort.Strings(expectedDeps["direct"])
 				}
 
 				convey.So(fileMod.deps, convey.ShouldResemble, expectedDeps)
 			})
+		})
+	})
+}
+
+func TestFileMod_getRequirements(t *testing.T) {
+	convey.Convey("Given a FileMod instance with dependencies", t, func() {
+		fileMod := &FileMod{
+			filePath:    "testdata/testmod.mod",
+			deps:        make(map[string][]string),
+			requirement: NewRequirement(),
+		}
+		fileMod.extractDataFromFile()
+
+		convey.Convey("When getRequirements() is called", func() {
+			err := fileMod.buildRequirements()
+			convey.Convey("it should not return an error", func() {
+				convey.So(err, convey.ShouldBeNil)
+			})
+
+		})
+
+		convey.Convey("It should add all non-internal dependecies to the requirement root", func() {
+			expectedRequirements := map[string][]string{
+				"root": {
+					"github.com/jackpal/bencode-go",
+					"github.com/spf13/viper",
+					"github.com/spf13/cobra",
+					"github.com/stretchr/testify",
+				},
+			}
+			if _, ok := expectedRequirements["root"]; ok {
+				sort.Strings(expectedRequirements["root"])
+			}
+
+			err := fileMod.buildRequirements()
+			convey.Convey("it should not return an error", func() {
+				convey.So(err, convey.ShouldBeNil)
+			})
+
+			actualRequirements, err := fileMod.getChilds()
+			if _, ok := actualRequirements["root"]; ok {
+				sort.Strings(actualRequirements["root"])
+			}
+
+			convey.Convey("It should add all non-internal dependencie to the requirement root", func() {
+				convey.So(err, convey.ShouldBeNil)
+				convey.So(actualRequirements, convey.ShouldResemble, expectedRequirements)
+			})
+
 		})
 	})
 }
